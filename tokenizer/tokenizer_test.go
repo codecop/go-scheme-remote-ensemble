8dff3c8f2671f0b468193e692fc6f43bdd6d5fa3
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-07-05 14:35:23 +0200

    add tokenizer package and tests

---
+++ tokenizer/tokenizer_test.go
@@ -0,0 +1,28 @@
+/* Tokenizer
+- input:
+    - string
+    - cleaned
+- output:
+    - list of tokens
+        - what type is it? (e.g. slice, linked list, interface, struct)
+        - brackets, identifiers (func names, variables), values (number, "string", boolean #t#f, other literals)
+        - ignore symbols
+        - (extend later if necessary)
+    - token (struct, interface)
+        - value (e.g. identifier -> string, number -> number, bracket -> nil)
+        - type
+*/
+package tokenizer_test
+
+import (
+	"testing"
+	"tokenizer"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyStringTokenizesIntoEmpty(t *testing.T) {
+	cleanedString := ""
+	tokens := tokenizer.Scan(cleanedString)
+	assert.Equal(t, make([]tokenizer.Token, 0), tokens)
+}
