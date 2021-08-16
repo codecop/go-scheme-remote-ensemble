0cfd3a003093dc635c7efac2cb85aeda4a5cee87
Peter Kofler <peter.kofler@code-cop.org>
2021-08-16 15:43:27 +0200

    Add 3 tests to make pass next time.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,9 +16,30 @@ func TestParses(t *testing.T) {
 		tokens      []tokenizer.Token
 		expectedAst parser.Ast
 	}{
+		// TODO make these tests pass
 		{
 			name:        "Empty tokens parses into empty",
 			tokens:      []tokenizer.Token{},
+			expectedAst: nil,
+		},
+		{
+			name: "Function evaluation (foo)",
+			tokens: []tokenizer.Token{
+				tokenizer.NewParenthesisToken("("),
+				tokenizer.NewNameToken("foo"),
+				tokenizer.NewParenthesisToken(")"),
+			},
+			expectedAst: nil,
+		},
+		{
+			name: "Function evaluation with arguments (+ 1 2)",
+			tokens: []tokenizer.Token{
+				tokenizer.NewParenthesisToken("("),
+				tokenizer.NewNameToken("plus"),
+				tokenizer.NewNumberToken("1"),
+				tokenizer.NewNumberToken("2"),
+				tokenizer.NewParenthesisToken(")"),
+			},
 			expectedAst: nil,
 		},
 	}
