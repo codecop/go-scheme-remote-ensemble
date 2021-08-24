32f02ab049668348f6411c1b5e34241ab7718689
Peter Kofler <peter.kofler@code-cop.org>
2021-08-24 14:00:36 +0200

    Remove premature parameterised test.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,49 +9,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParses(t *testing.T) {
+func TestEmptyTokensParseIntoEmpty(t *testing.T) {
 	assert := assert.New(t)
-	testCases := []struct {
-		name        string
-		tokens      []tokenizer.Token
-		expectedAst parser.Ast
-	}{
-		// TODO make these tests pass
-		{
-			name:        "Empty tokens parses into empty",
-			tokens:      []tokenizer.Token{},
-			expectedAst: parser.NewRoot(),
-		},
-		// root -> slice -> nodes -> slice -> boolean node
-		// {
-		// 	name: "Function evaluation (foo)",
-		// 	tokens: []tokenizer.Token{
-		// 		tokenizer.NewParenthesisToken("("),
-		// 		tokenizer.NewNameToken("foo"),
-		// 		tokenizer.NewParenthesisToken(")"),
-		// 	},
-		// 	expectedAst: nil,
-		// },
-		// {
-		// 	name: "Function evaluation with arguments (+ 1 2)",
-		// 	tokens: []tokenizer.Token{
-		// 		tokenizer.NewParenthesisToken("("),
-		// 		tokenizer.NewNameToken("plus"),
-		// 		tokenizer.NewNumberToken("1"),
-		// 		tokenizer.NewNumberToken("2"),
-		// 		tokenizer.NewParenthesisToken(")"),
-		// 	},
-		// 	expectedAst: nil,
-		// },
-	}
 
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			ast, err := parser.Parse(testCase.tokens)
-			assert.Equal(testCase.expectedAst, ast)
-			assert.NoError(err)
-		})
-	}
+	ast, err := parser.Parse([]tokenizer.Token{})
+
+	assert.Equal(parser.NewRoot(), ast)
+	assert.NoError(err)
 }
 
 func TestBooleanTokenParsesIntoBooleanNode(t *testing.T) {
@@ -65,3 +29,21 @@ func TestBooleanTokenParsesIntoBooleanNode(t *testing.T) {
 	// assert no other children
 	assert.NoError(err)
 }
+
+// TODO make these tests pass
+
+// 	name: "Function evaluation (foo)",
+// 	tokens: []tokenizer.Token{
+// 		tokenizer.NewParenthesisToken("("),
+// 		tokenizer.NewNameToken("foo"),
+// 		tokenizer.NewParenthesisToken(")"),
+// 	},
+
+// 	name: "Function evaluation with arguments (+ 1 2)",
+// 	tokens: []tokenizer.Token{
+// 		tokenizer.NewParenthesisToken("("),
+// 		tokenizer.NewNameToken("plus"),
+// 		tokenizer.NewNumberToken("1"),
+// 		tokenizer.NewNumberToken("2"),
+// 		tokenizer.NewParenthesisToken(")"),
+// 	},
