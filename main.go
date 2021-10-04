518cdd739cd3681d45b48410459663303aaeaad6
Paul Opuchlich <p.opuchlich@sportradar.com>
2021-10-04 14:23:02 +0200

    Add argument reading for integration tests.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"codecop.org/scheme/parser"
+	"codecop.org/scheme/tokenizer"
+)
 
 func main() {
+	flag.Parse()
+	args := flag.Args()
+	input, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		log.Fatalf("reading file err: %v", err)
+	}
+	tokens, err := tokenizer.Scan(string(input))
+	if err != nil {
+		log.Fatalf("tokenizer err: %v", err)
+	}
+	_, err = parser.Parse(tokens)
+	if err != nil {
+		log.Fatalf("parserizer err: %v", err)
+	}
 	fmt.Printf("test")
 }
